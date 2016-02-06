2eb634346330c2e8e674de6e9b668ea017e66886
hail-seitan <ianlondondu@gmail.com>
2016-02-05 20:37:31 -0500

    initial commit

---
+++ detectorists/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ForumThreadItem(scrapy.Item):
+    user_id = scrapy.Field()
+    timestamp = scrapy.Field()
+    message = scrapy.Field()
+    post_no = scrapy.Field()
+    # post_no is '2' in the last part of URL: http://metaldetectingforum.com/showpost.php?p=2499503&postcount=2
+    # post_no is shown on upper right of each post container.
+
+class UserItem(scrapy.Item):
+    user_id = scrapy.Field()
+    user_name = scrapy.Field()
---
+++ detectorists/spiders/detectorist_spider.py
@@ -0,0 +1,40 @@
+import scrapy
+
+from detectorists.items import ForumThreadItem
+
+class DetectoristSpider(scrapy.Spider):
+    name = 'detectorist'
+    allowed_domains = ['http://metaldetectingforum.com']
+    start_urls = ["http://metaldetectingforum.com/showthread.php?t=226343"]
+
+    def parse(self, response):
+        for post in response.xpath("//table[contains(@id,'post')]"):
+            t = ForumThreadItem()
+
+            t['user_id'] = post.xpath(".//a[@class='bigusername']/@href").re('u=(\d+)')[0]
+            t['timestamp'] = post.xpath("string(.//tr/td/div[@class='normal'][2])").extract()[0].strip()
+            t['message'] = post.xpath(".//*[contains(@id,'post_message_')]/text()").extract()[0]
+            t['post_no'] = post.xpath(".//tr/td/div[@class='normal'][1]/a//text()").extract()[0]
+
+            yield t
+
+
+# Post container: use this for each post
+# //table[contains(@id,'post')]
+
+# Name (<a> with link to member page): ... /a[@class='bigusername']
+# Get member page link URL with:
+# ... a[@class='bigusername']/@href
+
+# You could get name text with
+# ... a[@class='bigusername']/text()
+# but don't! Use userid to look up info later...
+
+# Post messages:
+# ... [contains(@id,'post_message_')]
+
+# Post timestamp
+# ... /tr/td/div[@class='normal'][2]
+
+# Post number
+# ... tr/td/div[@class='normal'][1]/a
